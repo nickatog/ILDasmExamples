018f6da9b9058d9a8b556be21cecbfcd2ddf0035
Nick Anderson <nanderson@ctuit.com>
2017-08-01 13:53:16 -0500

    Wrapping this up for now.

--- ILDasmExamples/Async.vb
+++ ILDasmExamples/Async.vb
@@ -3,6 +3,9 @@
         DoStuffAsync().Wait()
     End Sub
 
+    ' Async methods undergo some heavy compilation magic in order to look like a Task object to callers
+    ' Similar to lambda functions, an anonymous class will be generated to represent the body of the method
+    ' However, the class works like a state machine, and defines a MoveNext() method that is used to progress through the original method body
     Public Async Function DoStuffAsync() As Task
         Await Task.Delay(1000)
     End Function
