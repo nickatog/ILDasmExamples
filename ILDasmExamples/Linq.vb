691b18990ccc02d725523a477118111cd491be30
Nick Anderson <nanderson@ctuit.com>
2017-08-01 10:50:55 -0500

    Fleshing out examples for using blocks, LINQ, and lambdas.

---
+++ ILDasmExamples/Linq.vb
@@ -0,0 +1,17 @@
+﻿Public Module Linq
+    Public Sub SortOddNumbers(
+        ByVal numbers As IEnumerable(Of Integer))
+
+        ' LINQ query syntax looks simple on the surface, but really maps to a set of extension methods during compilation
+        ' This query results in 3 extension method calls (each of which takes the results of the previous calls as input to the next):
+        ' - IEnumerable.Where()
+        ' - IEnumerable.Select()
+        ' - IEnumerable.OrderBy()
+        ' Each method also takes a delegate as a parameter, which are created from anonymous functions defined at compile-time
+        Dim sortedNumbers =
+            From number As Integer In numbers
+            Where number Mod 2 = 1
+            Select number
+            Order By number
+    End Sub
+End Module
---
+++ ILDasmExamples/UsingBlocks.vb
@@ -0,0 +1,11 @@
+﻿Public Module UsingBlocks
+    Public Sub Main()
+        ' Using blocks can be used with any IDisposable object, and handle some tedious actions automatically and safely:
+        ' - Implicitly wraps the body in a try..finally
+        ' - Inside the finally block, calls IDisposable.Dispose() on the object if it's not null
+        Using output As New IO.FileStream("C:\test.txt", IO.FileMode.Create)
+            ' Do something with the stream
+            Console.WriteLine(output.CanWrite)
+        End Using
+    End Sub
+End Module
